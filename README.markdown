4752300c42e1bc359c31febfdfe1ea4a171f0f6c
Raynes <DiscipleRayne@gmail.com>
2010-11-06 21:18:31 -0500

    First commit.

---
+++ README.markdown
@@ -0,0 +1,1 @@
+This is my README. There are many like it, but this one is mine. My README isn't useful. My README will be filled in later. I will not abandon my README. My README will always serve me well.
---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject clojail "0.0.1-SNAPSHOT"
+  :description "An experimental sandboxing library."
+  :dependencies [[clojure "1.2.0"]
+                 [clojure-contrib "1.2.0"]]
+  :dev-dependencies [[swank-clojure "1.2.1"]])
