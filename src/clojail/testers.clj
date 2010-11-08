b0cf96705fa9e784a12067903534191d14b524de
Raynes <DiscipleRayne@gmail.com>
2010-11-08 04:23:26 -0600

    Added a predefined tester.

---
+++ src/clojail/testers.clj
@@ -0,0 +1,7 @@
+(ns ^{:doc "A set of predefined testers that you can use in your own sandboxes. I'm not promising that
+            any of these are really totally secure. I cannot possibly test these for everything."}
+  clojail.testers)
+
+(def ^{:doc "A somewhat secure tester. No promises."}
+     #{'alter-var-root 'intern 'def 'eval 'catch
+       'load-string 'load-reader 'addMethod 'ns-resolve})
