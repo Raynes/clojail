aba4d91d452a4c6fddaca99aaed19132784e6b51
Raynes <DiscipleRayne@gmail.com>
2010-11-08 04:23:08 -0600

    Added autodoc.

--- project.clj
+++ project.clj
@@ -2,4 +2,6 @@
   :description "An experimental sandboxing library."
   :dependencies [[clojure "1.2.0"]
                  [clojure-contrib "1.2.0"]]
-  :dev-dependencies [[swank-clojure "1.2.1"]])
+  :dev-dependencies [[swank-clojure "1.2.1"]
+                     [cake-autodoc "0.0.1-SNAPSHOT"]]
+  :tasks [cake-autodoc.tasks])
