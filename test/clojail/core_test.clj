bdec253388e9ef9c1817d3dd6fdb220a06ac9d54
Alan Malloy <alan@malloys.org>
2010-11-09 23:24:37 -0800

    Add some basic test for . interop and security

--- test/clojail/core_test.clj
+++ test/clojail/core_test.clj
@@ -3,6 +3,17 @@
         clojure.test))
 
 (def sb (sandbox secure-tester))
+(def easy (sandbox #{}))
 
 (deftest dot-test
-  (is (= true (sb '(= (.length "string") 6)))))
+  (is (= 4 (easy '(. "dots" (length))))))
+
+(deftest dot-shorthand-test
+  (is (= true (easy '(= (.length "string") 6)))))
+
+(deftest security-test
+  (is (= 7 (sb '(-> "bar.txt" java.io.File. .getName .length))))
+  (is (thrown? Exception (sb '(-> java.io.File .getMethods (aget 0) .getName)))))
+
+(deftest sandbox-config-test
+  (is (string? (easy '(-> java.io.File .getMethods (aget 0) .getName)))))
