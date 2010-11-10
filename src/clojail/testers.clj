0016dcb8ebc1b070c66eb054c12a23313cb95e56
Raynes <DiscipleRayne@gmail.com>
2010-11-10 00:17:43 -0600

    Added a function for creating package objects from strings for use in the sandbox, and put actual classes rather than symbols in the tester.

--- src/clojail/testers.clj
+++ src/clojail/testers.clj
@@ -2,8 +2,12 @@
             any of these are really totally secure. I cannot possibly test these for everything."}
   clojail.testers)
 
+(defn p
+  "Create a package object for putting in a tester."
+  [s] (Package/getPackage s))
+
 (def ^{:doc "A somewhat secure tester. No promises."}
      secure-tester
      #{'alter-var-root 'intern 'def 'eval 'catch
        'load-string 'load-reader 'addMethod 'ns-resolve
-       'clojure.lang.Ref 'clojure.lang.Reflector 'java.lang.reflect})
+       clojure.lang.Ref clojure.lang.Reflector (p "java.lang.reflect")})
