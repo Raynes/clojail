6c4c35d3b633243e8aad50bffd8327376914ec3a
Raynes <DiscipleRayne@gmail.com>
2010-11-10 02:00:00 -0600

    Added tests for whitelisting stuff.

--- test/clojail/core_test.clj
+++ test/clojail/core_test.clj
@@ -5,6 +5,9 @@
 (def sb (sandbox secure-tester))
 (def easy (sandbox #{}))
 
+(def wbsb (sandbox {:whitelist #{java.io.File java.lang.Math 'new 'clojure.core '+ '-}
+                    :blacklist #{'+ java.lang.Math}}))
+
 (deftest dot-test
   (is (= 4 (easy '(. "dots" (length))))))
 
@@ -17,3 +20,9 @@
 
 (deftest sandbox-config-test
   (is (string? (easy '(-> java.io.File .getMethods (aget 0) .getName)))))
+
+(deftest whitelist-test
+  (is (= 6 (wbsb '(- 12 6))))
+  (is (thrown? Exception (wbsb '(+ 3 3))))
+  (is (= (java.io.File. "") (wbsb '(java.io.File. ""))))
+  (is (thrown? Exception (wbsb '(java.lang.Math/abs 10)))))
