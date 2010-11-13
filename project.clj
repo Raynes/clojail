752a6d131dc4e17c43976ded70930898af9b81c7
Raynes <DiscipleRayne@gmail.com>
2010-11-12 22:33:01 -0600

    On to the next.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojail "0.1.0"
+(defproject clojail "0.2.0-SNAPSHOT"
   :description "An experimental sandboxing library."
   :dependencies [[clojure "1.2.0"]
                  [clojure-contrib "1.2.0"]]
