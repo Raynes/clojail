9d047bb8258adbe8c58f6a97458af99e29f5a61c
Alan Malloy <alan@malloys.org>
2010-12-27 16:30:16 -0800

    Finally fix the issue with clojail.core/tester being unbound when forcing lazy seqs outside the sandbox context in which they were created.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojail "0.2.0-SNAPSHOT"
+(defproject clojail "0.2.1-SNAPSHOT"
   :description "An experimental sandboxing library."
   :dependencies [[clojure "1.2.0"]
                  [clojure-contrib "1.2.0"]]
