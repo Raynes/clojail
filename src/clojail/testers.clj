3093d945ed8e137363a12a11805a7070a5f2b4fe
Alan Malloy <alan@malloys.org>
2011-01-14 12:49:58 -0800

    Add tomoj's suggestions to the tester list

--- src/clojail/testers.clj
+++ src/clojail/testers.clj
@@ -9,7 +9,8 @@
 (def ^{:doc "A somewhat secure tester. No promises."}
      secure-tester
      #{'alter-var-root 'intern 'def 'eval 'catch
-       'load-string 'load-reader 'addMethod 'ns-resolve 'resolve
+       'load-string 'load-reader 'addMethod 'ns-resolve 'resolve 'find-var
        '*read-eval* clojure.lang.Ref clojure.lang.Reflector 'ns-publics
        'ns-unmap 'set! 'ns-map 'ns-interns 'the-ns clojure.lang.Namespace
+       'push-thread-bindings 'pop-thread-bindings
        (p "java.lang.reflect")})
