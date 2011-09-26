57380e4e78fa9246f39a57629fcb19f94a7ed4d9
Alan Malloy <alan@malloys.org>
2011-09-26 11:48:47 -0700

    Desultory effort to get to 1.3

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
 (defproject clojail "0.4.0-SNAPSHOT"
   :description "An experimental sandboxing library."
   :dependencies [[clojure "1.2.0"]
-                 [clojure-contrib "1.2.0"]
                  [amalloy/utils "[0.3.7,)"]]
   :dev-dependencies [[swank-clojure "1.2.1"]
+                     [clojure-contrib "1.2.0"]
                      [cake-autodoc "0.0.1-SNAPSHOT"]]
   :tasks [cake-autodoc.tasks]
   :main clojail.main)
