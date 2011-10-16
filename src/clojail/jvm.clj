9c570fce390d4298105634111fb5f1959d05248b
Anthony Grimes <i@raynes.me>
2011-10-16 17:05:43 -0500

    Fix indentation and stupidity.

--- src/clojail/jvm.clj
+++ src/clojail/jvm.clj
@@ -4,24 +4,24 @@
 ; http://github.com/hiredman/clojurebot/blob/master/src/hiredman/sandbox.clj
 
 (defn empty-perms-list []
-      (doto (java.security.Permissions.)
-        (.add (RuntimePermission. "accessDeclaredMembers"))))
+  (doto (java.security.Permissions.)
+    (.add (RuntimePermission. "accessDeclaredMembers"))))
 
 
 (defn domain [perms]
-     (java.security.ProtectionDomain.
-       (java.security.CodeSource. nil
-                                  (cast java.security.cert.Certificate nil))
-       perms))
- 
-(defn context [dom]
-      (java.security.AccessControlContext. (into-array [dom])))
- 
+  (java.security.ProtectionDomain.
+   (java.security.CodeSource. nil
+                              (cast java.security.cert.Certificate nil))
+   perms))
+
+(defn context [& dom]
+  (java.security.AccessControlContext. (into-array dom)))
+
 (defn priv-action [thunk]
-      (proxy [java.security.PrivilegedAction] [] (run [] (thunk))))
- 
+  (proxy [java.security.PrivilegedAction] [] (run [] (thunk))))
+
 (defn jvm-sandbox
   [thunk context]
-      (java.security.AccessController/doPrivileged
-        (priv-action thunk)
-        context))
+  (java.security.AccessController/doPrivileged
+   (priv-action thunk)
+   context))
