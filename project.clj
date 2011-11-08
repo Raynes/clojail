a824ee8b45662171a2106081752ed94b2c3c4447
Anthony Grimes <i@raynes.me>
2011-11-08 16:27:12 -0600

    0.5.0

--- project.clj
+++ project.clj
@@ -1,3 +1,3 @@
-(defproject clojail "0.5.0-beta4"
-  :description "An experimental sandboxing library."
+(defproject clojail "0.5.0"
+  :description "A sandboxing library."
   :dependencies [[clojure "1.3.0"]])
