5eca69b66b24f1bb0ba6b39b7d3e461493b52760
Anthony Grimes <i@raynes.me>
2011-11-17 18:23:53 -0600

    Bump version.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojail "0.5.0"
+(defproject clojail "0.5.1"
   :description "A sandboxing library."
   :dependencies [[clojure "1.3.0"]]
   :dev-dependencies [[lein-multi "1.1.0-SNAPSHOT"]]
