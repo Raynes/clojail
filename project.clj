af2e547acb8b0af9cd1af3748b7fd4f115fde05c
Anthony Grimes <i@raynes.me>
2011-11-17 18:23:35 -0600

    Backwards compability with 1.2.1

--- project.clj
+++ project.clj
@@ -1,3 +1,5 @@
 (defproject clojail "0.5.0"
   :description "A sandboxing library."
-  :dependencies [[clojure "1.3.0"]])
+  :dependencies [[clojure "1.3.0"]]
+  :dev-dependencies [[lein-multi "1.1.0-SNAPSHOT"]]
+  :multi-deps {"1.2.1" [[clojure "1.2.1"]]})
