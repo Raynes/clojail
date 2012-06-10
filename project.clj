85418cffa4825f6b8d166d39b61b21fb95b32d7c
Anthony Grimes <i@raynes.me>
2012-06-10 04:37:03 -0500

    Update project.clj for lein 2 and add travis-ci stuff.

--- project.clj
+++ project.clj
@@ -1,5 +1,7 @@
 (defproject clojail "0.5.1"
   :description "A sandboxing library."
-  :dependencies [[clojure "1.3.0"]]
-  :dev-dependencies [[lein-multi "1.1.0-SNAPSHOT"]]
-  :multi-deps {"1.2.1" [[clojure "1.2.1"]]})
+  :dependencies [[org.clojure/clojure "1.4.0"]]
+  :aliases {"test-all" ["with-profile" "dev,1.2:dev,1.3:dev" "test"]}
+  :profiles {:1.2 {:dependencies [[org.clojure/clojure "1.2.1"]]}
+             :1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}}
+  :jvm-opts ["-Djava.security.policy=example.policy"])
