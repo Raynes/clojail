4570fd01ea51d025b8cd4e01def2fba9a4c2addd
Anthony Grimes <i@raynes.me>
2012-06-28 15:51:10 -0500

    We don't support 1.2.1 anymore.

--- project.clj
+++ project.clj
@@ -3,7 +3,6 @@
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [bultitude "0.1.6"]
                  [serializable-fn "1.1.3"]]
-  :aliases {"test-all" ["with-profile" "dev,1.2:dev,1.3:dev" "test"]}
-  :profiles {:1.2 {:dependencies [[org.clojure/clojure "1.2.1"]]}
-             :1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}}
+  :aliases {"test-all" ["with-profile" "dev,1.3:dev" "test"]}
+  :profiles {:1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}}
   :jvm-opts ["-Djava.security.policy=example.policy"])
