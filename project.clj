d5befaa0406952c428ddd4d019288474c0c749f0
Anthony Grimes <i@raynes.me>
2012-07-02 23:49:45 -0500

    Add URL and license keys to README.

--- project.clj
+++ project.clj
@@ -1,5 +1,8 @@
 (defproject clojail "0.6.0"
   :description "A sandboxing library."
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :url "https://github.com/flatland/clojail"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [bultitude "0.1.6"]
                  [serializable-fn "1.1.3"]]
