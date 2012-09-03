64adb140c4084b0ee63673c1c29e5cc58d2a6c9a
Anthony Grimes <i@raynes.me>
2012-09-03 16:02:52 -0500

    Release 1.0.0.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojail "0.6.2"
+(defproject clojail "1.0.0"
   :description "A sandboxing library."
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
@@ -6,6 +6,6 @@
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [bultitude "0.1.6"]
                  [serializable-fn "1.1.3"]]
-  :aliases {"test-all" ["with-profile" "dev,1.3:dev" "test"]}
+  :aliases {"testall" ["with-profile" "dev,1.3:dev" "test"]}
   :profiles {:1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}}
   :jvm-opts ["-Djava.security.policy=example.policy"])
