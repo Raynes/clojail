e791ce039e454fdb81db38731302e97efd1ebe9a
Anthony Grimes <i@raynes.me>
2013-02-22 20:29:48 -0800

    Inc version.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojail "1.0.4"
+(defproject clojail "1.0.5"
   :description "A sandboxing library."
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
