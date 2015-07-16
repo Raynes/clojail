699a2e526f0df92c37891d64b42fa0d51d99c84b
Yogthos <dmitri.sotnikov@uhn.on.ca>
2015-07-16 12:29:17 -0400

    updated test

--- test/clojail/core_test.clj
+++ test/clojail/core_test.clj
@@ -33,7 +33,7 @@
 
 (deftest macroexpand-test
   (is (= 'let (sb '(first '(let [x 1] x)))))
-  (is (= '(dec (clojure.core/-> x inc))
+  (is (= '(dec (inc x))
          (sb '(macroexpand '(-> x inc dec)))))
   (is (= 1 (sb '(-> 0 inc dec inc))))
   (is (= '(. "" length) (sb ''(. "" length)))))
