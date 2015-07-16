b750d2721ae6e2709f5ea49d08174ac9e01f6d83
Dmitri Sotnikov <dmitri.sotnikov@gmail.com>
2015-07-16 12:18:07 -0400

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
-(defproject clojail "1.0.6"
+(defproject clojail "1.0.7"
   :description "A sandboxing library."
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :url "https://github.com/flatland/clojail"
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [bultitude "0.1.6"]
-                 [serializable-fn "1.1.3"]
-                 [org.flatland/useful "0.9.3"]]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [bultitude "0.2.7"]
+                 [serializable-fn "1.1.4"]
+                 [org.flatland/useful "0.11.3"]]
   :aliases {"testall" ["with-profile" "dev,1.5:dev" "test"]}
-  :profiles {:1.5 {:dependencies [[org.clojure/clojure "1.5.0-RC16"]]}}
+  :profiles {:1.5 {:dependencies [[org.clojure/clojure "1.5.0"]]}}
   :jvm-opts ["-Djava.security.policy=example.policy"])
